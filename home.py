b0fa194855b464e8de9d190fe00a97d8e28c8e1b
Andrew Fandrianto <afandrianto@ebay.com>
2014-01-23 13:49:30 -0800

    adding url reading for typer

--- home.py
+++ home.py
@@ -3,11 +3,13 @@ from flask import Flask
 import the_periodic_typer_of_elements as typer
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
-    return typer.transform('hello world')
+    return typer.transform('hello world') + '<br><br><a href="typer/type.something.in.the.url.bar">' + typer.transform('click here') + '</a>'
 
-@app.route('/typer', methods=['GET'])
-def typer():
-    return typer.transform(request.args.get('q', 'type something in the "q" get parameter'))
+@app.route('/typer/<q>')
+def type(q):
+    return typer.transform(q)
+
